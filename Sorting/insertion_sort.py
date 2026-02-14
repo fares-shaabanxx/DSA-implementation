fa42239c22ec1a9d159a24d83adaa65bbef18ed6
Fares Shaaban <faresbenshaaban@gmail.com>
2026-02-14 08:11:06 +0200

    Add Insertion sort implementation

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr: list[int]):
+    for i in range(1, len(arr)):
+        key = arr[i] 
+        j = i - 1   
+        while j >= 0:
+            if arr[j] > key:    
+                arr[j+1] = arr[j]   
+                j -= 1  
+            else: break
+        arr[j+1] = key      
+
+
+arr = [9, 1, 5, 4]
+insertion_sort(arr)
+print(arr)
+        
